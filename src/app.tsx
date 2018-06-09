ebef2c8fae3f4db8a5e68c51e637e69b5f349ec3
Chris Baker <mail.chris.baker@gmail.com>
2018-06-09 03:42:08 +0000

    basic project structure
    Created basic project structure.

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+const App = () => <div id="app" />;
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
+import App from "./app";
+import reducers from "./reducers";
+
+const enhancer = devToolsEnhancer({});
+const store = createStore(reducers, enhancer);
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+render(<Root />, document.getElementById("root"));
